b146ab5ac1b1fff5d7cf267f15e45525b08f84f6
Shubhi Gupta <139733805+Shubhcyb2611@users.noreply.github.com>
2026-01-27 18:54:36 +0530

    Delete backend/dist directory

--- backend/dist/src/hotel/hotel.repository.d.ts
+++
@@ -1,6 +0,0 @@
-import { Room } from '@prisma/client';
-import { PrismaService } from '../../prisma/prisma.service';
-import { PrismaRepository } from '../common/repository/prisma.repository';
-export declare class HotelRepository extends PrismaRepository<Room> {
-    constructor(prisma: PrismaService);
-}
