622319c584e04b15518d4867edd0f38a16e7de82
Shubhcyb2611 <shubhigupta.2605@gmail.com>
2026-01-27 18:33:26 +0530

    feat/added complete hotel backned and frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
     app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'https://hotel-management-six-wheat.vercel.app',
     credentials: true,
   });
 await app.listen(3000);
